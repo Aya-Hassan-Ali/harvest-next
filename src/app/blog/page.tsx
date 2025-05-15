57a70576dda6cfa527d705ce57146de7e92f7a6e
ayahali96 <Ayahassan391996@gmail.com>
2025-05-16 02:30:25 +0300

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'FarmHub Blgo - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
+  title: 'HarvestGuard: Smart Farm Monitoring & Management Made Easy',
   description: 'HarvestGuard empowers you to efficiently track and manage your farm, acting as your vigilant eyes on the ground, ensuring everything runs smoothly.',
 };
 
