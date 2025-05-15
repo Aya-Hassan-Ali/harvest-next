d9b05b29436a6e40d3991de89e17a4738f40d1c9
ayahali96 <Ayahassan391996@gmail.com>
2025-05-16 02:34:45 +0300

    .

--- src/app/gallery-details/page.tsx
+++ src/app/gallery-details/page.tsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'FarmHub Gallery Details - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
+  title: 'HarvestGuard: Smart Farm Monitoring & Management Made Easy Gallery Details - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
   description: 'HarvestGuard empowers you to efficiently track and manage your farm, acting as your vigilant eyes on the ground, ensuring everything runs smoothly.',
 };
 
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'FarmHub Gallery - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
+  title: 'HarvestGuard: Smart Farm Monitoring & Management Made Easy Gallery - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
   description: 'HarvestGuard empowers you to efficiently track and manage your farm, acting as your vigilant eyes on the ground, ensuring everything runs smoothly.',
 };
 
