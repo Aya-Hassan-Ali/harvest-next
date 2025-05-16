90a000f451b4d441eb3773103fb622d45a7b0910
ayahali96 <Ayahassan391996@gmail.com>
2025-05-16 22:59:40 +0300

    .

--- src/app/blog-details/page.tsx
+++ src/app/blog-details/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'HarvestGuard Blog Details - HarvestGuard: Smart Farm Monitoring & Management Made Easy ',
+  title: ' HarvestGuard: Smart Farm Monitoring & Management Made Easy ',
   description: 'HarvestGuard empowers you to efficiently track and manage your farm, acting as your vigilant eyes on the ground, ensuring everything runs smoothly.',
 };
 
--- src/app/product-details/page.tsx
+++ src/app/product-details/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'HarvestGuard: Smart Farm Monitoring & Management Made Easy ProductDetails - HarvestGuard: Smart Farm Monitoring & Management Made Easy Next js Template',
+  title: 'HarvestGuard: Smart Farm Monitoring & Management Made Easy ',
   description: 'HarvestGuard empowers you to efficiently track and manage your farm, acting as your vigilant eyes on the ground, ensuring everything runs smoothly.',
 };
 
